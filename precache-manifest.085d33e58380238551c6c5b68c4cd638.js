af2cdc40ffb935344c6acbf195d851e770c4d6f6
unknown <mohamed.ahmed8412000@gmail.com>
2024-10-27 15:24:46 +0300

    Updates

---
+++ precache-manifest.085d33e58380238551c6c5b68c4cd638.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c370797a94f5a246d849d184bfce815",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ae640ff04d8592ee649c",
+    "url": "/portfolio/static/css/2.bce7b02d.chunk.css"
+  },
+  {
+    "revision": "b53c4aff01ee85bf09a1",
+    "url": "/portfolio/static/css/main.38ead654.chunk.css"
+  },
+  {
+    "revision": "ae640ff04d8592ee649c",
+    "url": "/portfolio/static/js/2.10eb32aa.chunk.js"
+  },
+  {
+    "revision": "f056774035ce39e6238293d1e83850f9",
+    "url": "/portfolio/static/js/2.10eb32aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b53c4aff01ee85bf09a1",
+    "url": "/portfolio/static/js/main.625c472f.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  },
+  {
+    "revision": "2796f6bb8bb611350ca9808fb4fb7492",
+    "url": "/portfolio/static/media/TempsImage.2796f6bb.jpg"
+  },
+  {
+    "revision": "712731ae5cb4a48b349c7928c75f1634",
+    "url": "/portfolio/static/media/avatar.712731ae.webp"
+  },
+  {
+    "revision": "4b78489cf814fc86a7713b2dabef535c",
+    "url": "/portfolio/static/media/cowenterImage.4b78489c.jpg"
+  },
+  {
+    "revision": "0ea2f971b9707efb4cf07ecf1de06586",
+    "url": "/portfolio/static/media/crochetArtImage.0ea2f971.jpg"
+  },
+  {
+    "revision": "c5014a6dba697799fbacd5a9c850de61",
+    "url": "/portfolio/static/media/full-nguoi-edit.c5014a6d.webp"
+  },
+  {
+    "revision": "c6fa148595cc1a5b56c3d9dc461902ea",
+    "url": "/portfolio/static/media/movieImage.c6fa1485.jpg"
+  },
+  {
+    "revision": "855e8ca01684f0d61e302ba09a177bfd",
+    "url": "/portfolio/static/media/no-image.855e8ca0.jpg"
+  },
+  {
+    "revision": "d0e2dd033232497f66aeef7923e839f7",
+    "url": "/portfolio/static/media/raqyahsteel.d0e2dd03.png"
+  },
+  {
+    "revision": "46932e23b962ad0995ad9f9778cb4509",
+    "url": "/portfolio/static/media/recipeImage.46932e23.jpg"
+  },
+  {
+    "revision": "f37ad303e270224a0c4a5b035f4cd2df",
+    "url": "/portfolio/static/media/safkaImage.f37ad303.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "e9cf3060c0b4f78a714bea00d562adbb",
+    "url": "/portfolio/static/media/todoappImage.e9cf3060.jpg"
+  },
+  {
+    "revision": "5e45eb0794a5872d4a88c27431a9910d",
+    "url": "/portfolio/static/media/todosImage.5e45eb07.jpg"
+  },
+  {
+    "revision": "d1f873c9894e9693fb3f3786d3d745ce",
+    "url": "/portfolio/static/media/troyCoffeImage.d1f873c9.jpg"
+  },
+  {
+    "revision": "2fcaaca21a14a358dda9caeafb2f3856",
+    "url": "/portfolio/static/media/weatherAppImage.2fcaaca2.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aed38f88149da972733fb0c3f7e59222.js"
+  "/portfolio/precache-manifest.085d33e58380238551c6c5b68c4cd638.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
